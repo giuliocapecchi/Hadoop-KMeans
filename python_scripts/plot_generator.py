2a1dad568bcaa13b08d3684be9c736fb2ea41a30
Giulio Capecchi <giuliocapecchi2000@gmail.com>
2023-06-20 19:45:24 +0200

    Added: - plot_generator for visualizing k-means iterations -python_scriptExecutor class -bugs resolved

--- python_scripts/plot_generator.py
+++ python_scripts/plot_generator.py
@@ -2,10 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-
 def calculate_distance(point, centroid):
     return np.linalg.norm(point - centroid)
-
 
 def assign_points_to_clusters(points, centroids):
     clusters = [[] for _ in range(len(centroids))]
@@ -15,19 +13,22 @@ def assign_points_to_clusters(points, centroids):
         clusters[closest_centroid_index].append(point)
     return clusters
 
-
-def plot_clusters(clusters, num_iter):
+def plot_clusters(clusters, num_iter, centroids):
     colors = ['red', 'blue', 'green', 'purple', 'orange', 'yellow']  # Add more colors if needed
-    for cluster_points, color in zip(clusters, colors):
+
+    for i, (cluster_points, color) in enumerate(zip(clusters, colors)):
         cluster_points = np.array(cluster_points)
         if len(cluster_points) > 0:
             plt.scatter(cluster_points[:, 0], cluster_points[:, 1], color=color)
+
+    centroids = np.array(centroids)
+    plt.scatter(centroids[:, 0], centroids[:, 1], color='black', marker='x', s=100)  # Plot centroids as black 'x'
+
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.title('K-means Clustering for iteration:'+num_iter)
+    plt.title('K-means Clustering for iteration: ' + num_iter)
     plt.savefig("./plots/plot_iter" + num_iter)
     plt.close()
-
 
 def parse_points_file(file_path):
     points = []
@@ -37,14 +38,12 @@ def parse_points_file(file_path):
             points.append([float(x), float(y)])
     return np.array(points)
 
-
 def parse_centroids(centroid_args):
     centroids = []
     for centroid_arg in centroid_args:
         x, y = centroid_arg.strip().split(',')
         centroids.append([float(x), float(y)])
     return np.array(centroids)
-
 
 def main():
     if len(sys.argv) < 4:
@@ -58,8 +57,7 @@ def main():
     centroids = parse_centroids(centroid_args)
 
     clusters = assign_points_to_clusters(points, centroids)
-    plot_clusters(clusters, num_iter)
-
+    plot_clusters(clusters, num_iter, centroids)
 
 if __name__ == '__main__':
     main()
